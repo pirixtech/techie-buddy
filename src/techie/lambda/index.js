d5e0d101236cbfa32cf404e70e12a1ee22328a14
Michael Hu <pxmyhdev@gmail.com>
2020-08-07 02:02:41 -0400

    remove swagger generated files

--- src/techie/lambda/index.js
+++ src/techie/lambda/index.js
@@ -17,28 +17,6 @@ const util = require('./utils/utils');
 const exerciseClient = require('./exercise-client');
 const { v4: uuidv4 } = require('uuid');
 
-
-var path = require('path');
-var http = require('http');
-
-var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
-
-// swaggerRouter configuration
-var options = {
-    controllers: path.join(__dirname, './controllers')
-};
-
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-expressAppConfig.addValidator();
-var app = expressAppConfig.getApp();
-
-// Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-});
-
 // API handler to get and return exercise routine
 const GetExerciseApiHandler = {
     canHandle(handlerInput) {
